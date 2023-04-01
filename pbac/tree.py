9d71c57af1d8f44c77cb4cdfd191f60791801545
akotkov <artjom.kotkov@yandex.ru>
2023-04-01 23:56:46 +0300

    Добавляем акторов и условие во все ветки дерева.

--- pbac/tree.py
+++ pbac/tree.py
@@ -24,17 +24,19 @@ class Rule(Executable):
         self._condition = condition
 
     def execute(self, subject: Any, target: Any, action: str, context: Any) -> Effect:
-        actors_decision = self._actors.execute(subject, target, action, context) if self._actors else Effect.NOT_APPLICABLE
-        if actors_decision is Effect.NOT_APPLICABLE or not actors_decision:
-            return Effect.NOT_APPLICABLE
+        if self._actors:
+            actors_decision = self._actors.execute(subject, target, action, context) if self._actors else Effect.NOT_APPLICABLE
+            if actors_decision is Effect.NOT_APPLICABLE or not actors_decision:
+                return Effect.NOT_APPLICABLE
 
-        try:
-            condition_decision = self._condition.execute(subject, target, action, context) if self._condition else Effect.NOT_APPLICABLE
-        except NotApplicable:
-            condition_decision = Effect.NOT_APPLICABLE
+        if self._condition:
+            try:
+                condition_decision = self._condition.execute(subject, target, action, context) if self._condition else Effect.NOT_APPLICABLE
+            except NotApplicable:
+                condition_decision = Effect.NOT_APPLICABLE
 
-        if condition_decision is Effect.NOT_APPLICABLE or not condition_decision:
-            return Effect.NOT_APPLICABLE
+            if condition_decision is Effect.NOT_APPLICABLE or not condition_decision:
+                return Effect.NOT_APPLICABLE
 
         return self._effect
 
@@ -57,18 +59,19 @@ class Policy(Executable):
         self._rules = rules if rules is not None else []
 
     def execute(self, subject: Any, target: Any, action: str, context: Any) -> Effect:
-        actors_decision = self._actors.execute(subject, target, action, context) if self._actors else Effect.NOT_APPLICABLE
+        if self._actors:
+            actors_decision = self._actors.execute(subject, target, action, context) if self._actors else Effect.NOT_APPLICABLE
 
-        if actors_decision is Effect.NOT_APPLICABLE or not actors_decision:
-            return Effect.NOT_APPLICABLE
+            if actors_decision is Effect.NOT_APPLICABLE or not actors_decision:
+                return Effect.NOT_APPLICABLE
+        if self._condition:
+            try:
+                condition_decision = self._condition.execute(subject, target, action, context) if self._condition else Effect.NOT_APPLICABLE
+            except NotApplicable:
+                condition_decision = Effect.NOT_APPLICABLE
 
-        try:
-            condition_decision = self._condition.execute(subject, target, action, context) if self._condition else Effect.NOT_APPLICABLE
-        except NotApplicable:
-            condition_decision = Effect.NOT_APPLICABLE
-
-        if condition_decision is Effect.NOT_APPLICABLE or not condition_decision:
-            return Effect.NOT_APPLICABLE
+            if condition_decision is Effect.NOT_APPLICABLE or not condition_decision:
+                return Effect.NOT_APPLICABLE
 
         return CombineResolver.combine(
             self._algorithm,
@@ -83,13 +86,32 @@ class Group(Executable):
         description: str,
         algorithm: Algorithm,
         items: Iterable[Policy | Rule | Self],
+        actors: Optional[ActorCombiner | ActorResolver] = None,
+        condition: Type[EntityCombiner | EntityAttribute] = None,
     ):
         self._id = _id
         self._description = description
         self._algorithm = algorithm
         self._items = items
+        self._actors = actors
+        self._condition = condition
 
     def execute(self, subject: Any, target: Any, action: str, context: Any):
+        if self._actors:
+            actors_decision = self._actors.execute(subject, target, action, context) if self._actors else Effect.NOT_APPLICABLE
+
+            if actors_decision is Effect.NOT_APPLICABLE or not actors_decision:
+                return Effect.NOT_APPLICABLE
+
+        if self._condition:
+            try:
+                condition_decision = self._condition.execute(subject, target, action, context) if self._condition else Effect.NOT_APPLICABLE
+            except NotApplicable:
+                condition_decision = Effect.NOT_APPLICABLE
+
+            if condition_decision is Effect.NOT_APPLICABLE or not condition_decision:
+                return Effect.NOT_APPLICABLE
+
         return CombineResolver.combine(
             self._algorithm,
             (item.execute(subject, target, action, context) for item in self._items),
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbac',
-    version='0.0.1',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[
         'pydantic==1.10.6',
